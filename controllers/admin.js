df00d478fba6a20e10381aa53fa34aff282050b5
Hy-Quynh <hyquynh123@gmail.com>
2021-08-26 23:59:43 +0700

    done admin router

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const User = require('../model/users');
+const Booking = require('../model/booking');
 const Cinemas = require('../model/cinemas');
 const Cineplexes = require('../model/cineplexes');
 const Movies = require('../model/movies');
@@ -6,27 +9,41 @@ const Showtimes = require('../model/showtime');
 
 module.exports = {
   HOME: asyncHandler(async (req, res) => {
-    res.render('admin/admin-dashboard');
+    res.render('admin/dashboard');
   }),
-
-  GET_ADD_CINEPLEX: asyncHandler(async (req, res) => {
-    res.render('admin/pages/cinema-complexes/add-new-cineplex');
+  CINEPLEXES_REVENUE: asyncHandler(async (req, res) => {
+    const { start, end } = req.body;
+    const from = new Date(start);
+    const to  = new Date(end);
+    if(to < from) return res.status(400).json({ status: 'error', message: 'The end date must be greater than the start date.' });
+    const revenue = await Cineplexes.cineplexesRevenue(start, end);
+    if(!revenue || revenue.length == 0) return res.status(400).json({ status: 'empty', message: 'No data currently available.' });
+    res.status(200).json({
+      status: 'success',
+      cineplexes_revenue: revenue
+    })
   }),
-
-  GET_LIST_CINEPLEXES: asyncHandler(async (req, res) => {
+  MOVIES_REVENUE: asyncHandler(async (req, res) => {
+    const { start, end } = req.body;
+    const from = new Date(start);
+    const to  = new Date(end);
+    if(to < from) return res.status(400).json({ status: 'error', message: 'The end date must be greater than the start date.' });
+    const revenue = await Movies.moviesRevenue(start, end);
+    if(!revenue || revenue.length == 0) return res.status(400).json({ status: 'empty', message: 'No data currently available.' });
+    res.status(200).json({
+      status: 'success',
+      movies_revenue: revenue
+    })
+  }),
+  GET_CINEPLEXES_LIST: asyncHandler(async (req, res) => {
     const cineplexes = await Cineplexes.getAll();
-    res.render('admin/pages/cinema-complexes/list-cineplexes',{
+    res.render('admin/pages/cinema-complexes/cineplexes-list',{
       cineplexes_list: cineplexes
     });
   }),
-
-  DELETE_CINEPLEX: asyncHandler(async (req, res) => {
-    const { cineplexId } = req.body;
-    const hidden = await Cineplexes.hiddenCineplex(cineplexId);
-    if(hidden[0].status == true) return res.status(400).json({ message: 'Delete failed!' });
-    res.status(200).json({ status: 'success' });
+  GET_ADD_CINEPLEX: asyncHandler(async (req, res) => {
+    res.render('admin/pages/cinema-complexes/add-cineplex');
   }),
-
   ADD_NEW_CINEPLEX: asyncHandler(async (req, res) => {
     const { name_cineplex, address } = req.body;
     const new_cineplex = {
@@ -37,15 +54,24 @@ module.exports = {
     if(!added_cineplex[0]) return res.status(400).json({ message: 'Add failed!'});
     res.status(200).json({ status: 'success' });
   }),
-
-  GET_LIST_CINEMAS: asyncHandler(async (req, res) => {
+  DELETE_CINEPLEX: asyncHandler(async (req, res) => {
+    const { cineplexId } = req.body;
+    const hidden = await Cineplexes.hiddenCineplex(cineplexId);
+    if(hidden[0].status == true) return res.status(400).json({ message: 'Delete failed!' });
+    res.status(200).json({ status: 'success' });
+  }),
+  GET_CINEMAS_LIST: asyncHandler(async (req, res) => {
     const cinemas = await Cinemas.getAll();
-    res.render('admin/pages/cinemas/list_cinemas', {
+    res.render('admin/pages/cinemas/cinemas-list', {
       cinemas_list: cinemas
     });
   }),
-
-
+  GET_ADD_CINEMA: asyncHandler(async (req, res) => {
+    const cineplexes = await Cineplexes.getAll();
+    res.render('admin/pages/cinemas/add-cinema', {
+      cineplexes_list: cineplexes
+    });
+  }),
   ADD_NEW_CINEMA: asyncHandler(async (req, res) => {
     const { name_cinema, cineplexId, type, horizontal_size, vertical_size } = req.body;
     const new_cinema = {
@@ -59,32 +85,21 @@ module.exports = {
     if(!added_cinema[0]) return res.status(400).json({ message: 'Add failed!'});
     res.status(200).json({ status: 'success' });
   }),
-
-  GET_ADD_CINEMA: asyncHandler(async (req, res) => {
-    const cineplexes = await Cineplexes.getAll();
-    res.render('admin/pages/cinemas/add-new-cinema', {
-      cineplexes_list: cineplexes
-    });
-  }),
-
   DELETE_CINEMA: asyncHandler(async (req, res) => {
     const { cinemaId } = req.body;
     const hidden = await Cinemas.hiddenCinema(cinemaId);
     if(hidden[0].status == true) return res.status(400).json({ message: 'Delete failed!' });
     res.status(200).json({ status: 'success' });
   }),
-
-  GET_LIST_MOVIES: asyncHandler(async (req, res) => {
+  GET_MOVIES_LIST: asyncHandler(async (req, res) => {
     const movies = await Movies.getAll();
-    res.render('admin/pages/movies/list-movies',{
+    res.render('admin/pages/movies/movies-list',{
       movies_list: movies
     });
   }),
-
   GET_ADD_MOVIE: asyncHandler(async (req, res) => {
     res.render('admin/pages/movies/add-movie');
   }),
-
   ADD_NEW_MOVIE: asyncHandler(async (req, res) => {
     const { name, open, runtime, trailer, genre, introduction, language } = req.body;
     if(!req.file) return res.render('admin/pages/movies/add-movie',{ status: 'error', message: 'Please upload picture' });
@@ -103,12 +118,55 @@ module.exports = {
     if(!added_movie[0]) return res.render('admin/pages/movies/add-movie',{ status: 'error', message: 'Add failed' });
     res.render('admin/pages/movies/add-movie',{ status: 'success' });
   }),
-
   DELETE_MOVIE: asyncHandler(async (req, res) => {
     const { movieId } = req.body;
     const hidden = await Movies.hiddenMovie(movieId);
     if(hidden[0].status == true) return res.status(400).json({ message: 'Delete failed!' });
     res.status(200).json({ status: 'success' });
   }),
-
+  GET_SHOWTIMES_LIST: asyncHandler(async (req, res) => {
+    const showtimes = await Showtimes.getAll();
+    res.render('admin/pages/showtimes/showtimes-list',{
+      showtimes_list: showtimes
+    });
+  }),
+  GET_ADD_SHOWTIME: asyncHandler(async (req, res) => {
+    const cineplexes = await Cineplexes.getCineplexName();
+    const movies = await Movies.getMovieName();
+    res.render('admin/pages/showtimes/add-showtime',{
+      cineplexes_list: cineplexes,
+      movies_list: movies
+    });
+  }),
+  GET_CINEMA_BY_CINEPLEX: asyncHandler(async (req, res) => {
+    const { cineplex_id } = req.query;
+    const list = await Cinemas.getCinemaName(cineplex_id);
+    res.status(200).json({ cinemas_list: list });
+  }),
+  ADD_NEW_SHOWTIME: asyncHandler(async (req, res) => {
+    const { cinema_id, movie_id, type, start_time, end_time, fare, show_at} = req.body;
+    const new_showtime = {
+      cinemaId: cinema_id,
+      movieId: movie_id,
+      cinemaType: type,
+      startTime: start_time,
+      endTime: end_time,
+      fare: fare,
+      showAt: show_at
+    };
+    const added_showtime = await Showtimes.addNewShowtime(new_showtime);
+    if(!added_showtime[0]) return res.status(400).json({ message: 'Add failed!'});
+    res.status(200).json({ status: 'success' });
+  }),
+  DELETE_SHOWTIME: asyncHandler(async (req, res) => {
+    const { cinemaId, movieId } = req.body;
+    const hidden = await Showtimes.hiddenShowtime(cinemaId, movieId);
+    if(hidden[0].status == true) return res.status(400).json({ message: 'Delete failed!' });
+    res.status(200).json({ status: 'success' });
+  }),
+  LOGOUT: asyncHandler(async (req, res) => {
+    const { userID } = req.session;
+    delete req.session.userID;
+    res.redirect('/');
+  })
 };
