c0f86d0cb147f9fc175aa81e62fd0c76e1afaa66
Hy-Quynh <hyquynh123@gmail.com>
2021-08-27 00:00:44 +0700

    done admin router

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const Logged = require('../middlewares/ensure-logged-in');
 const adminController = require('../controllers/admin');
+const asyncHandler = require('express-async-handler');
 const multer  = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
@@ -8,26 +9,38 @@ const router = express.Router();
 // loggged
 router.use(Logged.ensureLoggedInAdmin);
 
-/**
- *Home
- */
-router.get('/list-cinemas', adminController.GET_LIST_CINAMAS);
-router.get('/admin-dashboard', adminController.ADMIN_HOME);
- /**
-  * Cineplexes
-  */
-router.get('/list-cineplexes', adminController.GET_LIST_CINEPLEXES);
-router.get('/add-new-cineplex', adminController.GET_ADD_CINEPLEX);
-router.post('/add-new-cineplex', adminController.ADD_NEW_CINEPLEX);
+
+Home
+router.get('/dashboard', adminController.HOME);
+router.post('/cineplexes-revenue', adminController.CINEPLEXES_REVENUE);
+router.post('/movies-revenue', adminController.MOVIES_REVENUE);
+// Cineplexes
+router.get('/cineplexes-list', adminController.GET_CINEPLEXES_LIST);
+router.get('/add-cineplex', adminController.GET_ADD_CINEPLEX);
+router.post('/add-cineplex', adminController.ADD_NEW_CINEPLEX);
+router.post('/delete-cineplex', adminController.DELETE_CINEPLEX);
+cinemas
+router.get('/cinemas-list', adminController.GET_CINEMAS_LIST);
+router.get('/add-cinema', adminController.GET_ADD_CINEMA);
+router.post('/add-cinema', adminController.ADD_NEW_CINEMA);
 router.post('/delete-cinema', adminController.DELETE_CINEMA);
+// Movies
+router.get('/movies-list', adminController.GET_MOVIES_LIST);
+router.get('/add-movie', adminController.GET_ADD_MOVIE);
+router.post('/add-movie', upload.single('picture'), adminController.ADD_NEW_MOVIE);
+router.post('/delete-movie', adminController.DELETE_MOVIE);
+// Showtimes
+router.get('/showtimes-list', adminController.GET_SHOWTIMES_LIST);
+router.get('/add-showtime', adminController.GET_ADD_SHOWTIME);
+router.post('/add-showtime', adminController.ADD_NEW_SHOWTIME);
+router.post('/delete-showtime', adminController.DELETE_SHOWTIME);
+router.get('/cinema-by-cineplex', adminController.GET_CINEMA_BY_CINEPLEX);
 
-/** 
- * Movie
- */
 
-router.get('/list-movies', adminController.GET_LIST_MOVIES);
-router.get('/add-new-movie', adminController.GET_ADD_MOVIE);
-router.post('/add-new-movie', upload.single('picture'), adminController.ADD_NEW_MOVIE);
 
+Logout
+router.get('/logout', adminController.LOGOUT);
 module.exports = router;
 
+
+/*# sourceMappingURL=detail.css.map */
